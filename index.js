0a97d01609bca216ce8801634b32c4528836a939
tokeshwar <tokeshwar007@gmail.com>
2022-09-23 15:45:56 +0530

    done

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('home')
+});
+
+server.listen(3000, () => {
+    console.log('Express App is running on port 3000')
+});
+
+io.on('connection', (socket) => {
+    console.log('new user connected')
+
+    socket.on('message', (data) => {
+        socket.broadcast.emit('message', data)
+    })
+})
